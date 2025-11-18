0ede4707d62060403bfe65ce9fc3941c6a8a0615
weloooooo <ahmedwael005@gmail.com>
2025-11-18 23:56:41 +0200

    login,register and dashboard ui

--- Phase1/UniversityMangmentSystem(rooms)/src/main/java/org/example/universitymangmentsystem/controllers/LoginController.java
+++ Phase1/UniversityMangmentSystem(rooms)/src/main/java/org/example/universitymangmentsystem/controllers/LoginController.java
@@ -1,12 +1,19 @@
-package org.example.universitymangmentsystem.controllers;
+package edu.facilities.model;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -34,8 +41,8 @@ public class LoginController {
     }
 
     @FXML
-    void register(ActionEvent event) {
-        Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+    void register(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
