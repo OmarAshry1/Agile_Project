758f37f4cd0d2640c73344e69b46608199deed62
Meena Maged <meena.maged3@gmail.com>
2026-08-14 20:52:47 +0300

    T2.2.1 T2.2.2 T2.2.3 feat(enrollment): implement core-course registration

---
+++ src/main/java/edu/facilities/model/EnrollmentStatus.java
@@ -0,0 +1,12 @@
+package edu.facilities.model;
+
+/**
+ * Enum representing the status of a student enrollment
+ */
+public enum EnrollmentStatus {
+    ENROLLED,   // Currently enrolled
+    DROPPED,    // Dropped the course
+    COMPLETED,  // Completed the course
+    FAILED      // Failed the course
+}
+
